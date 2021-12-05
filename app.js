0e62310087c31325289ebbc36512a8fdc281c006
Bhavesh <bhaveshjagtap21@gmail.com>
2021-12-05 16:46:36 +0530

    Javascript Work Done

--- app.js
+++ app.js
@@ -1,26 +1,87 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 
-main();
+function getCompChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random() * 3));
+    return choices[randomNumber];
+}
 
-function main() {
-    rock_div.addEventListener('click', function () {
-        game("r");
-    })
-
-    paper_div.addEventListener('click', function () {
-        game("p");
-    })
-
-    scissor_div.addEventListener('click', function () {
-        game("s");
-    })
+function convertToText(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    if (letter === "s") return "Scissor";
 
 }
+
+function win(u_Choice, c_Choice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    // result_p.innerHTML = convertToText(u_Choice) + " beats " + convertToText(c_Choice) + ". You win ! ";
+    result_p.innerHTML = `${convertToText(u_Choice)}  beats ${convertToText(c_Choice)} . You win ! `;
+
+}
+
+function lose(u_Choice, c_Choice) {
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    // result_p.innerHTML = convertToText(c_Choice) + " loses to  " + convertToText(u_Choice) + ". You lose ! ";
+    result_p.innerHTML = `${convertToText(u_Choice)}  loses to  ${convertToText(c_Choice)} . You lose ! `;
+}
+
+    function draw(u_Choice, c_Choice) {
+        userScore_span.innerHTML = userScore;
+        compScore_span.innerHTML = compScore;
+        // result_p.innerHTML = convertToText(u_Choice) + " equal to " + convertToText(c_Choice) + ". It's a Draw ! ";
+        result_p.innerHTML = `${convertToText(u_Choice)}  equal to ${convertToText(c_Choice)} . It's a Draw ! `;
+    }
+
+
+
+
+    function game(userChoice) {
+        const compChoice = getCompChoice();
+        switch (userChoice + compChoice) {
+            case "rs":
+            case "pr":
+            case "sp":
+                win(userChoice,compChoice);
+                break;
+            case "rp":
+            case "ps":
+            case "sr":
+                lose(userChoice,compChoice);
+                break;
+            case "rr":
+            case "pp":
+            case "ss":
+                draw(userChoice,compChoice);
+                break;
+        }
+
+    }
+
+    function main() {
+        rock_div.addEventListener('click', function () {
+            game("r");
+        })
+
+        paper_div.addEventListener('click', function () {
+            game("p");
+        })
+
+        scissor_div.addEventListener('click', function () {
+            game("s");
+        })
+
+    }
+    main();
